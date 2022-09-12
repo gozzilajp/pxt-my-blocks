ff6ebfa622944c0937e300db2df8f009a1ae0411
koki <gozzilajp@gmail.com>
2022-09-13 08:19:00 +0900

    change test.ts

--- test.ts
+++ test.ts
@@ -1,1 +1,13 @@
 // tests go here; this will not be compiled when this package is used as an extension.
+basic.showString('test');
+basic.forever(function() {
+	const temp = input.temperature();
+	const fValue = myBolocks.convertTemperature(temp, TempConvertType.CtoF);
+	const cValue = myBolocks.convertTemperature(temp, TempConvertType.FtoC);
+	basic.showString('T:');
+	basic.showNumber(temp);
+	basic.showString('C:');
+	basic.showNumber(cValue);
+	basic.showString('F:');
+	basic.showNumber(fValue);
+});
